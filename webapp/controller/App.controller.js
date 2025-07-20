2e5a5c3f2dd620b987928ab2eae29e525ad679c8
Joao Pereira <joao.pereira@stratesys-ts.com>
2025-07-20 20:25:05 +0000

    Initial commit - UI5 Personal Profile app

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("databinding.databindingproject.controller.App", {
+      onInit() {
+      }
+  });
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["databinding/databindingproject/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
